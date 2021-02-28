c69e4a0470734015d80db2bc3a5d487c66501820
Stella Veski <stella.veski@softwire.com>
2021-02-28 19:33:06 +0000

    Add icons.

---
+++ src/components/shared/play-icon.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const PlayIcon = () => (
+  <svg width="80px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="0.5"/>
+    <path d="M10 16.5v-9l6 4.5-6 4.5z" fill="black"/>
+  </svg>
+);
