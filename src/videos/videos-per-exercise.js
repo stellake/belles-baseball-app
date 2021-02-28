d3933955a0e3b7e3fd4cc2823d5f651d460d01a8
Stella Veski <stella.veski@softwire.com>
2021-02-28 18:52:19 +0000

    Some very basic content.

---
+++ src/videos/videos-per-exercise.js
@@ -0,0 +1,89 @@
+import SecondBase1 from '../videos/second-base-1.mp4';
+import SecondBase2 from '../videos/second-base-2.mp4';
+import SecondBase3 from '../videos/second-base-3.mp4';
+import SecondBase4 from '../videos/second-base-4.mp4';
+import SecondBase5 from '../videos/second-base-5.mp4';
+import SecondBase6 from '../videos/second-base-6.mp4';
+import SecondBase7 from '../videos/second-base-7.mp4';
+import ThirdBase1 from '../videos/second-base-1.mp4';
+import ThirdBase2 from '../videos/second-base-2.mp4';
+import ThirdBase3 from '../videos/second-base-3.mp4';
+import ThirdBase4 from '../videos/second-base-4.mp4';
+import ThirdBase5 from '../videos/second-base-5.mp4';
+import {EXERCISE_KEYS} from '../exercises';
+
+const standardGoOption = (isCorrectAnswer) => ({
+  text: 'Go!',
+  isCorrectAnswer
+});
+
+const standardWaitOption = (isCorrectAnswer) => ({
+  text: 'Wait...',
+  isCorrectAnswer
+});
+
+// TODO: Sort out the correct/incorrect options
+const basicSecondBaseVideos = [
+  {
+    src: SecondBase1,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  },
+  {
+    src: SecondBase2,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  },
+  {
+    src: SecondBase3,
+    options: [standardGoOption(false), standardWaitOption(true)]
+  },
+  {
+    src: SecondBase4,
+    options: [standardGoOption(false), standardWaitOption(true)]
+  }
+];
+
+const intermediateSecondBaseVideos = [
+  ...basicSecondBaseVideos,
+  {
+    src: SecondBase5,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  },
+  {
+    src: SecondBase6,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  },
+  {
+    src: SecondBase7,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  }
+];
+
+const thirdBaseVideos = [
+  {
+    src: ThirdBase1,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  },
+  {
+    src: ThirdBase2,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  },
+  {
+    src: ThirdBase3,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  },
+  {
+    src: ThirdBase4,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  },
+  {
+    src: ThirdBase5,
+    options: [standardGoOption(true), standardWaitOption(false)]
+  }
+];
+
+
+export const videosByExercise = {
+  [EXERCISE_KEYS.BASIC_SECOND_BASE]: basicSecondBaseVideos,
+  [EXERCISE_KEYS.INTERMEDIATE_SECOND_BASE]: intermediateSecondBaseVideos,
+  [EXERCISE_KEYS.BASIC_THIRD_BASE]: thirdBaseVideos
+};
